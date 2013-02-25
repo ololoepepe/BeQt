16eb649fe15de335a31b8a41eeba702773413b38
Andrey Bogdanov <the-dark-angel@yandex.ru>
2013-02-25 21:07:39 +0600

    Add convenience functions to BeQt namespace

--- src/corelib/bnamespace.cpp
+++ src/corelib/bnamespace.cpp
@@ -13,6 +13,7 @@
 #include <QObject>
 #include <QList>
 #include <QPair>
+#include <QSysInfo>
 
 namespace BeQt
 {
@@ -148,5 +149,96 @@ int execProcess(const QString &workingDir, const QString &command, const QString
         in.setCodec(codec);
     return (proc.exitStatus() == QProcess::NormalExit) ? bRet(output, in.readAll(), proc.exitCode()) : -1;
 }
+
+#if defined(Q_OS_MAC)
+B_CORE_EXPORT QString macVersionToString(QSysInfo::MacVersion version)
+{
+    switch (version)
+    {
+    case QSysInfo::MV_9:
+        return "Mac OS 9";
+    case QSysInfo::MV_10_0:
+    case QSysInfo::MV_CHEETAH:
+        return "Mac OS X 10.0 Cheetah";
+    case QSysInfo::MV_10_1:
+    case QSysInfo::MV_PUMA:
+        return "Mac OS X 10.1 Puma";
+    case QSysInfo::MV_10_2:
+    case QSysInfo::MV_JAGUAR:
+        return "Mac OS X 10.2 Jaguar";
+    case QSysInfo::MV_10_3:
+    case QSysInfo::MV_PANTHER:
+        return "Mac OS X 10.3 Panther";
+    case QSysInfo::MV_10_4:
+    case QSysInfo::MV_TIGER:
+        return "Mac OS X 10.4 Tiger";
+    case QSysInfo::MV_10_5:
+    case QSysInfo::MV_LEOPARD:
+        return "Mac OS X 10.5 Leopard";
+    case QSysInfo::MV_10_6:
+    case QSysInfo::MV_SNOWLEOPARD:
+        return "Mac OS X 10.6 Snow Leopard";
+    case QSysInfo::MV_10_7:
+    case QSysInfo::MV_LION:
+        return "Mac OS X 10.7 Lion";
+    case QSysInfo::MV_10_8:
+    case QSysInfo::MV_MOUNTAINLION:
+        return "Mac OS X 10.8 Mountain Lion";
+    default:
+        return "Unknown";
+    }
+}
+
+B_CORE_EXPORT QString macVersionString()
+{
+    return macVersionToString(QSysInfo::macVersion());
+}
+#endif
+
+#if defined(Q_OS_WIN)
+B_CORE_EXPORT QString windowsVersionToString(QSysInfo::WinVersion version)
+{
+    switch (version)
+    {
+    case QSysInfo::WV_32s:
+        return "Windows 3.1";
+    case QSysInfo::WV_95:
+        return "Windows 95";
+    case QSysInfo::WV_98:
+        return "Windows 98";
+    case QSysInfo::WV_Me:
+        return "Windows Me";
+    case QSysInfo::WV_NT:
+    case QSysInfo::WV_4_0:
+        return "Windows NT (4.0)";
+    case QSysInfo::WV_2000:
+    case QSysInfo::WV_5_0:
+        return "Windows 2000 (5.0)";
+    case QSysInfo::WV_XP:
+    case QSysInfo::WV_5_1:
+        return "Windows XP (5.1)";
+    case QSysInfo::WV_2003:
+    case QSysInfo::WV_5_2:
+        return "Windows Server 2003 / Windows Server 2003 R2 / "
+                "Windows Home Server / Windows XP Professional x64 (5.2)";
+    case QSysInfo::WV_VISTA:
+    case QSysInfo::WV_6_0:
+        return "Windows Vista / Windows Server 2008 (6.0)";
+    case QSysInfo::WV_WINDOWS7:
+    case QSysInfo::WV_6_1:
+        return "Windows 7 / Windows Server 2008 R2 (6.1)";
+    case QSysInfo::WV_WINDOWS8:
+    case QSysInfo::WV_6_2:
+        return "Windows 8 (6.2)";
+    default:
+        return "Unknown";
+    }
+}
+
+B_CORE_EXPORT QString windowsVersionString()
+{
+    return windowsVersionToString(QSysInfo::windowsVersion());
+}
+#endif
 
 }
--- src/corelib/bnamespace.h
+++ src/corelib/bnamespace.h
@@ -13,6 +13,7 @@ class QObject;
 #include <QtGlobal>
 #include <QList>
 #include <QPair>
+#include <QSysInfo>
 
 /*============================================================================
 ================================ BeQt ========================================
@@ -47,6 +48,14 @@ B_CORE_EXPORT int execProcess(const QString &command, const QStringList &argumen
                               int startTimeout, int finishTimeout, QString *output = 0, QTextCodec *codec = 0);
 B_CORE_EXPORT int execProcess(const QString &workingDir, const QString &command, const QStringList &arguments,
                               int startTimeout, int finishTimeout, QString *output = 0, QTextCodec *codec = 0);
+#if defined(Q_OS_MAC)
+B_CORE_EXPORT QString macVersionToString(QSysInfo::MacVersion version);
+B_CORE_EXPORT QString macVersionString();
+#endif
+#if defined(Q_OS_WIN)
+B_CORE_EXPORT QString windowsVersionToString(QSysInfo::WinVersion version);
+B_CORE_EXPORT QString windowsVersionString();
+#endif
 
 }
 
