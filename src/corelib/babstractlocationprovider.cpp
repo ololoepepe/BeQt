b8df6802b67a6a32f8df42a865e876f7da51c9e8
Andrey Bogdanov <ololoepepe@gmail.com>
2014-11-27 17:19:59 +0500

    Document BAbstractLocationProvider

--- src/corelib/babstractlocationprovider.cpp
+++ src/corelib/babstractlocationprovider.cpp
@@ -25,12 +25,44 @@
 ================================ BAbstractLocationProvider ===================
 ============================================================================*/
 
+/*!
+    \class BAbstractLocationProvider
+    \brief The BAbstractLocationProvider class is the abstract base class of
+    location providers, which are used to provide information about
+    application resources.
+
+    This class implements an abstract location provider. Subclasses of this
+    class handle application resources locations, test their existance and
+    create corresponding directories.
+
+    BAbstractLocationProvider instances are registered in BApplicationBase
+    (specifically in BCoreApplication or BApplication) by calling
+    BApplicationBase::installLocationProvider or
+    BApplicationBase::installLocationProviders. The application then uses
+    installed location providers to resolve paths to resources requested by
+    BApplicationBase::location or BApplicationBase::locations call.
+
+    Example:
+
+    \snippet src/corelib/babstractapplicationprovider.cpp 0
+
+    \sa BLocationProvider
+*/
+
 /*============================== Public constructors =======================*/
+
+/*!
+    Constructs an abstract location provider.
+*/
 
 BAbstractLocationProvider::BAbstractLocationProvider()
 {
     //
 }
+
+/*!
+    Destroys the location provider.
+*/
 
 BAbstractLocationProvider::~BAbstractLocationProvider()
 {
@@ -39,10 +71,61 @@ BAbstractLocationProvider::~BAbstractLocationProvider()
 
 /*============================== Public methods ============================*/
 
+/*!
+    \fn QStringList BAbstractLocationProvider::locationNames()
+
+    Returns a list of location names (identifiers) supported by this location
+    provider.
+
+    Reimplement this function when creating a subclass of
+    BAbstractLocationProvider.
+
+    \sa locationPath()
+*/
+
+/*!
+    \fn QString BAbstractLocationProvider::locationPath(
+            const QString &locationName, BApplicationBase::ResourceType type)
+
+    Returns the path associated with \a locationName for the resource type
+    \a type.
+
+    This function should return empty QString if the specified
+    \a locationName is not supported by this location provider, or if there
+    is no resource for specified \a type.
+
+    Reimplement this function when creating a subclass of
+    BAbstractLocationProvider.
+
+    \sa locationNames()
+*/
+
+/*!
+    This virtual function is called by BApplicationBase to determine if the
+    location provider can create path specified by \a locationName for
+    resource type \a type.
+
+    Default implementation returns false.
+
+    \sa createLocationPath()
+*/
+
 bool BAbstractLocationProvider::canCreateLocationPath(const QString &, BApplicationBase::ResourceType) const
 {
     return false;
 }
+
+/*!
+    This virtual function is called by BApplicationBase to create the path
+    specified by \a locationName for resource type \a type.
+
+    Should return true if the location exists or has been successfully
+    created. Otherwise should return false.
+
+    Default implementation does nothing and returns false.
+
+    \sa canCreateLocationPath()
+*/
 
 bool BAbstractLocationProvider::createLocationPath(const QString &, BApplicationBase::ResourceType)
 {
