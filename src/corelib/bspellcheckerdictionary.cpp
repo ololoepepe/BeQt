ed48df32f118f453e47b215ad9eb827f57f24977
Andrey Bogdanov <ololoepepe@gmail.com>
2014-12-25 11:35:54 +0500

    Document BSpellCheckerDictionary

--- src/corelib/bspellcheckerdictionary.cpp
+++ src/corelib/bspellcheckerdictionary.cpp
@@ -147,13 +147,33 @@ bool BSpellCheckerDictionaryPrivate::testValidity()
 ================================ BSpellCheckerDictionary =====================
 ============================================================================*/
 
+/*!
+\class BSpellCheckerDictionary
+\inmodule BeQtCore
+\brief The BSpellCheckerDictionary class provides a dictionary for spell checking.
+
+The BSpellCheckerDictionary is a wrapper over a Hunspell dictionary. It is used to check spelling of words.
+
+\sa BSpellChecker
+*/
+
 /*============================== Public constructors =======================*/
+
+/*!
+Constructs a dictionary and sets the path to dictionary directory to \a path. The dictionary is loaded automatically
+from that directory. The locale is set according to the directory name.
+*/
 
 BSpellCheckerDictionary::BSpellCheckerDictionary(const QString &path) :
     BBase(*new BSpellCheckerDictionaryPrivate(this, path))
 {
     d_func()->init();
 }
+
+/*!
+Constructs a dictionary and sets the underlying Hunspell dictionary affix data to \a affixData and dictionary data to
+\a dictionaryData. The locale is set to the locale with name \a locale.
+*/
 
 BSpellCheckerDictionary::BSpellCheckerDictionary(const QByteArray &affixData, const QByteArray &dictionaryData,
                                                  const QString &locale) :
@@ -162,12 +182,23 @@ BSpellCheckerDictionary::BSpellCheckerDictionary(const QByteArray &affixData, co
     d_func()->init();
 }
 
+/*!
+Constructs a dictionary and sets the underlying Hunspell dictionary affix data to \a affixData and dictionary data to
+\a dictionaryData. The locale is set to \a locale.
+*/
+
 BSpellCheckerDictionary::BSpellCheckerDictionary(const QByteArray &affixData, const QByteArray &dictionaryData,
                                                  const QLocale &locale) :
     BBase(*new BSpellCheckerDictionaryPrivate(this, "", affixData, dictionaryData, locale.name()))
 {
     d_func()->init();
 }
+
+/*!
+Destroys the object, deleting the associated data object.
+
+The underlying Hunspell dictionary is also deleted.
+*/
 
 BSpellCheckerDictionary::~BSpellCheckerDictionary()
 {
@@ -176,10 +207,27 @@ BSpellCheckerDictionary::~BSpellCheckerDictionary()
 
 /*============================== Static public methods =====================*/
 
+/*!
+Returns a map of the characters (typically letters) which should be replaced in a string before checking spelling. This
+is useful, for example, for such cases as Russian 'ё' letter, which must be treated as 'е' for Hunspell to work
+correctly.
+
+Some of such letter replacements are hardcoded into BeQt library (e.g. the above mentioned 'ё').
+
+\sa setReplacedLetters()
+*/
+
 QMap<QChar, QChar> BSpellCheckerDictionary::replacedLetters()
 {
     return BSpellCheckerDictionaryPrivate::replacedLetters;
 }
+
+/*!
+Sets the map of the characters (typically letters) which should be replaced in a string before checking spelling to
+\a m.
+
+\sa replacedLetters()
+*/
 
 void BSpellCheckerDictionary::setReplacedLetters(const QMap<QChar, QChar> &m)
 {
@@ -187,6 +235,10 @@ void BSpellCheckerDictionary::setReplacedLetters(const QMap<QChar, QChar> &m)
 }
 
 /*============================== Public methods ============================*/
+
+/*!
+Adds the \a word to this dictionary. That word will be treated as correct when calling spell().
+*/
 
 void BSpellCheckerDictionary::addWord(const QString &word)
 {
@@ -196,45 +248,88 @@ void BSpellCheckerDictionary::addWord(const QString &word)
      d_func()->hunspell->add(w);
 }
 
+/*!
+Returns the raw affix data of the dictionary.
+*/
+
 QByteArray BSpellCheckerDictionary::affixData() const
 {
     return d_func()->AffixData;
 }
+
+/*!
+Returns a pointer to the QTextCodec used to decode the dictionary.
+
+For an invalid BSpellCheckerDictionary instance this function returns 0.
+*/
 
 QTextCodec *BSpellCheckerDictionary::codec() const
 {
     return d_func()->codec;
 }
 
+/*!
+Returns the name of the QTextCodec used to decode the dictionary.
+
+For an invalid BSpellCheckerDictionary instance this function returns 0.
+*/
+
 QString BSpellCheckerDictionary::codecName() const
 {
     return d_func()->codec ? QString::fromLatin1(d_func()->codec->name()) : QString();
 }
+
+/*!
+Returns the raw dictionary data of the dictionary.
+*/
 
 QByteArray BSpellCheckerDictionary::dictionaryData() const
 {
     return d_func()->DictionaryData;
 }
 
+/*!
+Returns true if this dictionary instance is valid (i.e. the underlying Hunspell dictionary is valid). Otherwise returns
+false.
+*/
+
 bool BSpellCheckerDictionary::isValid() const
 {
     return d_func()->hunspell;
 }
+
+/*!
+Returns the locale of the dictionary.
+*/
 
 QLocale BSpellCheckerDictionary::locale() const
 {
     return d_func()->Locale;
 }
 
+/*!
+Returns the name of the locale of the dictionary.
+*/
+
 QString BSpellCheckerDictionary::localeName() const
 {
     return isValid() ? d_func()->Locale.name() : QString();
 }
 
+/*!
+Returns the path to the directory from which this dictionary was loaded.
+
+If the dictionary was loaded from raw data, this function returns an empty QString.
+*/
+
 QString BSpellCheckerDictionary::path() const
 {
     return d_func()->Path;
 }
+
+/*!
+Removes the \a word from this dictionary. That word will be treated as incorrect when calling spell().
+*/
 
 void BSpellCheckerDictionary::removeWord(const QString &word)
 {
@@ -244,6 +339,11 @@ void BSpellCheckerDictionary::removeWord(const QString &word)
      d_func()->hunspell->remove(w);
 }
 
+/*!
+Checks if the \a word is correct (i.e. the dictionary contains it). In case the word is correct, returns true;
+otherwise returns false.
+*/
+
 bool BSpellCheckerDictionary::spell(const QString &word) const
 {
     if (!isValid())
@@ -251,6 +351,11 @@ bool BSpellCheckerDictionary::spell(const QString &word) const
     const char *w = codec()->fromUnicode(BSpellCheckerDictionaryPrivate::replaceSpecialLetters(word)).constData();
     return d_func()->hunspell->spell(w);
 }
+
+/*!
+Checks if all the \a words are correct (i.e. the dictionary contains them all). In case all the words are correct,
+returns true. If any word is incorrect, returns false.
+*/
 
 bool BSpellCheckerDictionary::spell(const QStringList &words) const
 {
@@ -260,6 +365,15 @@ bool BSpellCheckerDictionary::spell(const QStringList &words) const
     }
     return true;
 }
+
+/*!
+Returns a list of the words resembling the given \a word.
+
+This function may be used to suggest a list of possible variants of corrections for a word written incorrectly.
+
+For example, for the incorrectly written word "spon" the following words will be suggested: "soon", "spin", "son",
+"spoon", "pons", "span", "spot", "spun", "upon", "sponge".
+*/
 
 QStringList BSpellCheckerDictionary::suggest(const QString &word) const
 {
